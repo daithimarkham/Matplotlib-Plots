4c45838824fa0beb8224831067218de213865f9d
David Markham <davidmarkham76@gmail.com>
2019-09-26 18:01:44 +0100

    More text within a Histogram.

---
+++ Hist.text.2.py
@@ -0,0 +1,21 @@
+# David Markham
+# 29/09/2018
+
+# More text within Histograms. 
+
+import matplotlib.pyplot as plt 
+import numpy as np
+
+ax = plt.subplot(111)
+
+t = np.arange(0.0, 5.0, 0.01)
+s = np.cos(2*np.pi*t)
+line, = plt.plot(t, s, lw=2)
+
+# Displays the max with an arrow pointing towards it. 
+plt.annotate('local max', xy=(2, 1), xytext=(3, 1.5),
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             )
+
+plt.ylim(-2, 2)
+plt.show()

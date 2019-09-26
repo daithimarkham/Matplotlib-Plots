23ca3879ed39b97b6664ce4159bfb00036817204
David Markham <davidmarkham76@gmail.com>
2019-09-26 18:30:22 +0100

    Created Non-Linear Axes.

---
+++ NonLinear.axes.py
@@ -0,0 +1,73 @@
+# David Markham
+# 26/09/2019
+# Logarithmic and other nonlinear axes.
+
+
+
+# matplotlib.pyplot supports not only linear axis scales, but also logarithmic and logit scales. 
+# This is commonly used if data spans many orders of magnitude.
+# Log only allows positive values, and lets you choose how to handle negative ones ( mask or clip ).
+# Symlog means symmetrical log, and allows positive and negative values.
+# Symlog allows to set a range around zero within the plot will be linear instead of logarithmic.
+
+
+# Changing the scale of an axis is easy:
+# plt.xscale('log')
+
+# An example of four plots with the same data and different scales for the y axis is shown below.
+
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+from matplotlib.ticker import NullFormatter  # useful for `logit` scale
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# make up some data in the interval ]0, 1[
+y = np.random.normal(loc=0.5, scale=0.4, size=1000)
+y = y[(y > 0) & (y < 1)]
+y.sort()
+x = np.arange(len(y))
+
+# plot with various axes scales
+plt.figure()
+
+# linear
+plt.subplot(221)
+plt.plot(x, y)
+plt.yscale('linear')
+plt.title('linear')
+plt.grid(True)
+
+
+# log
+plt.subplot(222)
+plt.plot(x, y)
+plt.yscale('log')
+plt.title('log')
+plt.grid(True)
+
+
+# symmetric log
+plt.subplot(223)
+plt.plot(x, y - y.mean())
+plt.yscale('symlog', linthreshy=0.01)
+plt.title('symlog')
+plt.grid(True)
+
+# logit
+plt.subplot(224)
+plt.plot(x, y)
+plt.yscale('logit')
+plt.title('logit')
+plt.grid(True)
+# Format the minor tick labels of the y-axis into empty strings with
+# `NullFormatter`, to avoid cumbering the axis with too many labels.
+plt.gca().yaxis.set_minor_formatter(NullFormatter())
+# Adjust the subplot layout, because the logit one may take more space
+# than usual, due to y-tick labels like "1 - 10^{-3}"
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                    wspace=0.35)
+
+plt.show()
